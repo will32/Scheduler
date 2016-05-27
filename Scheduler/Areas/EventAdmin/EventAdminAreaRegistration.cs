abc9d2d1ef145247c39cb23f05cfeb1de8e60fc1
William Lin <linzhijun032@yahoo.com>
2016-05-27 14:59:35 -0700

    Add project files.

---
+++ Scheduler/Areas/EventAdmin/EventAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Scheduler.Areas.EventAdmin
+{
+    public class EventAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "EventAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "EventAdmin_default",
+                "EventAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Scheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Scheduler.Startup))]
+namespace Scheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
